e6bd4e0757034bcb4ccf7cdfc455dd69f8af1f9a
LAC <lac@lee-anne-clarke.com>
2018-02-17 15:07:57 -0500

    Header changes
    Removed unused styles/content; fixed tabs

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,27 +4,19 @@ import '../styles.css'
 
 
 function Header() {
-	let styles = {
-		display: 'none'
-	}
-
 	return (  
 		<header className="header">
-	        <h1>Lee-Anne Clarke</h1>
-	        <h2 className="jobtitle">Front-End Web Developer</h2>
-	        <h3 className="myemail">lac @ lee-anne-clarke.com</h3>
-	        
-	        <div className="divider">
-	             <a className="header__link header__link--left" href="https://github.com/lee-anne-clarke/codes" target="blank">
-	             	<i className="fa fa-github"><span>Github</span></i>
-				</a>
+			<h1>Lee-Anne Clarke</h1>
+			<h2 className="jobtitle">Front-End Web Developer</h2>
+			<h3 className="myemail">lac @ lee-anne-clarke.com</h3>
 
-	             <a style={styles} className="header__link header__link--right" href="http://lee-anne-clarke.com/blog" target="blank">
-	             	<i className="fa fa-pencil"><span>Blog</span></i>
-				</a>				
-	         </div>
-	    </header>
-    );
+			<div className="divider">
+				<a className="header__link header__link--left" href="https://github.com/lee-anne-clarke/" target="blank">
+					<i className="fa fa-github"><span>Github</span></i>
+				</a>			
+			</div>
+		</header>
+	);
 }
 
 
