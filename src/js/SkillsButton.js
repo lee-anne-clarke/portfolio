5c36f32c78d8a70e623c7f3b4c1422c07955b773
LAC <lac@lee-anne-clarke.com>
2018-03-01 23:21:38 -0500

    Skills Button update
    Changed component class to SFC

--- src/js/SkillsButton.js
+++ src/js/SkillsButton.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 import '../normalize.min.css'
 import '../styles.css'
 
 
-class SkillsButton extends Component {
-	render() {
-		return (
-			<button className={`skills__btn ${this.props.classes}`}>
-				<span>{this.props.text}</span>
-			</button>
-		);
-	}
+function SkillsButton(props) {
+	return (
+		<button className={`skills__btn ${props.classes}`}>
+			<span>{props.text}</span>
+		</button>
+	);
 }
 
 
