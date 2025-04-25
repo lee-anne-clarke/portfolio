c55ea0b889d8b65f42e0fb50943e87c21d0fad3c
LAC <lee-anne-clarke@users.noreply.github.com>
2025-04-24 21:59:22 -0400

    Updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,20 @@
-import React, { useEffect } from 'react';
-import Scroll from 'react-scroll';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
-
-// variables for React Scroll
-let Events     = Scroll.Events;
-let scroll     = Scroll.animateScroll;
-let scrollSpy  = Scroll.scrollSpy;
-let scroller   = Scroll.scroller;
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 
 export default function Header() {
-	useEffect(() => {
-		scrollSpy.update();
-
-		return() => {
-			Events.scrollEvent.remove('begin');
-			Events.scrollEvent.remove('end');
-		};
-
-	}, []);
-
-	const scrollToTop = () => {
-		scroll.scrollToTop();
-	}
-
-	const scrollToAbout = () => {
-		scroller.scrollTo('aboutAnchor', {smooth: true,});
-	}
-
 	return (  
 		<header className="header">
 			<h1>Lee-Anne Clarke</h1>
-			<p className="header__item header__item--jobtitle">Front-End Software Engineer</p>
-			<p className="header__item header__item--email">lac @ lee-anne-clarke.com</p>
-			<p>
-				<a className="btn--header" href="https://github.com/lee-anne-clarke/" target="blank">github</a>
-			</p>
+			<p className="header__text header__text--jobtitle">Front-End Software Engineer</p>
+			<p className="header__text header__text--email">lac @ lee-anne-clarke.com</p>
 
-			<div className="divider">
-				<button className="btn" onClick={scrollToAbout}>
-					<FontAwesomeIcon className="" icon={faArrowDown} />
-					<span className="u-sr">Scroll down</span>
-				</button>			
+			<div className="header__btn-wrap">
+				<a className="btn--header" href="https://github.com/lee-anne-clarke/" title="Github" target="blank">
+					<FontAwesomeIcon icon={faGithub} />
+					<span className="u-sr">My Github</span>
+				</a>			
 			</div>
 		</header>
 	);
